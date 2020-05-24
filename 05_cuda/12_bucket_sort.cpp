ef73a7056981f3f39419012225e3e331b471f93b
Wiem Ben Rim <whb04@mail.aub.edu>
2020-05-25 05:51:48 +0900

    Update 12_bucket_sort.cpp

--- 05_cuda/12_bucket_sort.cpp
+++ 05_cuda/12_bucket_sort.cpp
@@ -2,28 +2,47 @@
 #include <cstdlib>
 #include <vector>
 
+__global__ void sort(int *bucket, int *key, int range) {
+  int j = blockIdx.x * blockDim.x + threadIdx.x;
+  atomicAdd(&bucket[key[j]], 1);
+  
+  __syncthreads(); //wait for buckets to be assigned 
+
+  int sum = 0; //variable to contain bucket values
+  for(int i=0;;i++){ //values in key array
+    if (sum<=j){ //sum controls how much to let i increase and the offset of adding it to key array
+      key[j]=i; //assign values one by one, starting from i=0. Stop adding if the bucket is empty
+      sum+=bucket[i]; 
+      bucket[i]--; 
+    }
+    else{
+      return;
+    }
+  }
+}
+
 int main() {
   int n = 50;
   int range = 5;
-  std::vector<int> key(n);
+  //change key declaration
+  int *key;
+  cudaMallocManaged(&key, n*sizeof(int));
+  //same initialization code
   for (int i=0; i<n; i++) {
     key[i] = rand() % range;
     printf("%d ",key[i]);
   }
   printf("\n");
 
-  std::vector<int> bucket(range); 
-  for (int i=0; i<range; i++) {
+ //change bucket declaration
+ int *bucket;
+ cudaMallocManaged(&bucket, range*sizeof(int));
+ for (int i=0; i<range; i++) {
     bucket[i] = 0;
   }
-  for (int i=0; i<n; i++) {
-    bucket[key[i]]++;
-  }
-  for (int i=0, j=0; i<range; i++) {
-    for (; bucket[i]>0; bucket[i]--) {
-      key[j++] = i;
-    }
-  }
+  //call sort on n threads
+  sort<<<1,n>>>(bucket,key, range);
+  cudaDeviceSynchronize(); //does not work without waiting here
 
   for (int i=0; i<n; i++) {
     printf("%d ",key[i]);
